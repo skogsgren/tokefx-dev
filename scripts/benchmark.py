809bf073dd17e288b4044c7784d9d157256f5b12
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2026-02-18 15:01:43 +0100

    move util functions to dedicated package since they're reused

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from pathlib import Path
 
 from icecream import ic
@@ -6,22 +7,34 @@ import toml
 import torch
 
 from tokefx.benchmarks.belebele import belebele
+from tokefx.utils import load_config, read_json, set_json
 
-cfg = toml.load("scripts/config.toml")
+cfg = load_config("scripts/config.toml")
 models = cfg["eval"]["models"]
-DATADIR = Path(cfg["data"]["BELEBELE_BASE"])
 
-metrics: dict = {t: {} for _, t in models}
+metrics_file_out = cfg["dir"]["out"] / "benchmark_original.json"
+if not metrics_file_out.parent.exists():
+    metrics_file_out.parent.mkdir(parents=True)
+
+metrics = read_json(metrics_file_out)
+for _, t in models:
+    if t not in metrics:
+        metrics[t] = {}
+
 for model, tokenizer in models:
-    for lang, spec in cfg["data"]["lang"].items():
+    for lang, spec in cfg["lang"].items():
+        if metrics[tokenizer].get(spec["iso639"]):
+            print(f"already evaluated {spec['iso639']} ({model}, {tokenizer})")
+            continue
         print(f"testing {spec['iso639']} for ({model}, {tokenizer})")
-        belebele_fp = DATADIR / (spec["iso639"] + ".jsonl")
+        belebele_fp = cfg["dir"]["belebele_base"] / (spec["iso639"] + ".jsonl")
         belebele_score = belebele(
             belebele_fp,
             model,
             tokenizer,
             device=torch.device("cuda"),
-            max_seq_len=512,
+            max_seq_len=4096,
         )
         metrics[tokenizer][spec["iso639"]] = belebele_score
+        set_json(metrics_file_out, metrics)
         ic(metrics)
---
+++ tokefx/utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import os
+from pathlib import Path
+
+from deepmerge import always_merger
+import json
+import toml
+
+from icecream import ic
+
+
+def load_config(cfg_path: Path | str) -> dict:
+    cfg = toml.load(cfg_path)
+    # we want directories to be Path object for iteration
+    for k, v in cfg["dir"].items():
+        cfg["dir"][k] = Path(os.path.expandvars(v))
+    return cfg
+
+
+def read_json(json_path: Path | str) -> dict:
+    json_path = Path(json_path)
+    if json_path.exists():
+        return json.loads(json_path.read_text())
+    else:
+        return {}
+
+
+def set_json(json_path: Path | str, new_data: dict):
+    data = read_json(json_path)
+    json_path.write_text(
+        json.dumps(
+            always_merger.merge(data, new_data),
+            indent=2,
+        )
+    )
