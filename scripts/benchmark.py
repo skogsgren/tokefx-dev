d6cb059dc0a7340609ca2183d2dc53d20db4dddc
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2026-02-19 20:50:30 +0100

    add draft for xcopa and xnli benchmark

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -2,31 +2,40 @@
 import os
 from pathlib import Path
 
-from icecream import ic
+import iso639
 import toml
 import torch
 
 from tokefx.benchmarks.belebele import belebele
+from tokefx.benchmarks.xcopa import xcopa
+from tokefx.benchmarks.xnli import xnli
 from tokefx.utils import load_config, read_json, set_json
+
+from icecream import ic
 
 cfg = load_config("scripts/config.toml")
 models = cfg["eval"]["models"]
 
-metrics_file_out = cfg["dir"]["out"] / "benchmark_original.json"
-if not metrics_file_out.parent.exists():
-    metrics_file_out.parent.mkdir(parents=True)
 
-metrics = read_json(metrics_file_out)
-for _, t in models:
-    if t not in metrics:
-        metrics[t] = {}
+def initialize_metrics_file(out: Path):
+    if not out.parent.exists():
+        out.parent.mkdir(parents=True)
+    metrics = read_json(out)
+    for _, t in models:
+        if t not in metrics:
+            metrics[t] = {}
+    return metrics
 
+
+# == belebele ==
+belebele_out_fp = cfg["dir"]["out"] / "benchmark_belebele_original.json"
+belebele_metrics = initialize_metrics_file(belebele_out_fp)
 for model, tokenizer in models:
     for lang, spec in cfg["lang"].items():
-        if metrics[tokenizer].get(spec["iso639"]):
+        if belebele_metrics[tokenizer].get(spec["iso639"]):
             print(f"already evaluated {spec['iso639']} ({model}, {tokenizer})")
             continue
-        print(f"testing {spec['iso639']} for ({model}, {tokenizer})")
+        print(f"testing belebele {spec['iso639']} for ({model}, {tokenizer})")
         belebele_fp = cfg["dir"]["belebele_base"] / (spec["iso639"] + ".jsonl")
         belebele_score = belebele(
             belebele_fp,
@@ -35,6 +44,48 @@ for model, tokenizer in models:
             device=torch.device("cuda"),
             max_seq_len=4096,
         )
-        metrics[tokenizer][spec["iso639"]] = belebele_score
-        set_json(metrics_file_out, metrics)
-        ic(metrics)
+        belebele_metrics[tokenizer][spec["iso639"]] = belebele_score
+        set_json(belebele_out_fp, belebele_metrics)
+        ic(belebele_metrics)
+
+# == xcopa ==
+xcopa_out_fp = cfg["dir"]["out"] / "benchmark_xcopa_original.json"
+xcopa_metrics = initialize_metrics_file(xcopa_out_fp)
+for model, tokenizer in models:
+    for lang, spec in cfg["lang"].items():
+        if xcopa_metrics[tokenizer].get(spec["iso639"]):
+            print(f"already evaluated {spec['iso639']} ({model}, {tokenizer})")
+            continue
+        print(f"testing xcopa {spec['iso639']} for ({model}, {tokenizer})")
+
+        xcopa_score = xcopa(
+            iso639.Language.from_part3(spec["iso639"][:3]).part1,
+            model,
+            tokenizer,
+            device=torch.device("cuda"),
+            max_seq_len=4096,
+        )
+        xcopa_metrics[tokenizer][spec["iso639"]] = xcopa_score
+        set_json(xcopa_out_fp, xcopa_metrics)
+        ic(xcopa_metrics)
+
+# == xnli ==
+xnli_out_fp = cfg["dir"]["out"] / "benchmark_xnli_original.json"
+xnli_metrics = initialize_metrics_file(xnli_out_fp)
+for model, tokenizer in models:
+    for lang, spec in cfg["lang"].items():
+        if xnli_metrics[tokenizer].get(spec["iso639"]):
+            print(f"already evaluated {spec['iso639']} ({model}, {tokenizer})")
+            continue
+        print(f"testing xnli {spec['iso639']} for ({model}, {tokenizer})")
+
+        xnli_score = xnli(
+            iso639.Language.from_part3(spec["iso639"][:3]).part1,
+            model,
+            tokenizer,
+            device=torch.device("cuda"),
+            max_seq_len=4096,
+        )
+        xnli_metrics[tokenizer][spec["iso639"]] = xnli_score
+        set_json(xnli_out_fp, xnli_metrics)
+        ic(xnli_metrics)
---
+++ tokefx/benchmarks/xcopa.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import json
+
+import codecs
+from datasets import load_dataset
+import iso639
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+from icecream import ic
+
+from tokefx.constants import SPECIAL_TOKEN_RULES
+
+
+def xcopa(
+    lang: str,
+    model_spec: str,
+    tokenizer_spec: str,
+    device: torch.device = torch.device("cpu"),
+    max_seq_len: int = 4096,
+) -> float:
+    if lang == "en":
+        data = load_dataset("pkavumba/balanced-copa")["test"]
+    else:
+        data = load_dataset("cambridgeltl/xcopa", lang)["test"]
+    model = AutoModelForCausalLM.from_pretrained(model_spec).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_spec)
+    pid = -100 if tokenizer.pad_token_id is None else tokenizer.pad_token_id
+
+    corr = []
+    loss = nn.CrossEntropyLoss(ignore_index=pid, reduction="none")
+
+    for r in tqdm(data):
+        prefix = r["premise"].strip() + " "
+        mc_texts = []
+        for choice_i in range(2):
+            mc_texts.append(prefix + r[f"choice{choice_i + 1}"].strip())
+        corr_i = int(r["label"])
+        option_losses = torch.zeros(len(mc_texts))
+        for mc_i, mc_text in enumerate(mc_texts):
+            inputs = tokenizer([mc_text], add_special_tokens=True)
+            input_ids = inputs["input_ids"]
+            attention_mask = inputs["attention_mask"]
+            if len(input_ids[0]) > max_seq_len:
+                input_ids[0] = input_ids[0][:max_seq_len]
+                attention_mask[0] = attention_mask[0][:max_seq_len]
+            input_ids = torch.tensor(input_ids).to(device)
+            attention_mask = torch.tensor(attention_mask).to(device)
+            outputs = model(
+                input_ids=input_ids,
+                attention_mask=attention_mask,
+                return_dict=True,
+            )
+            logits = outputs["logits"].detach()
+            del outputs
+            labels = input_ids[:, 1:]
+            logits = logits[:, :-1, :]
+            logits = torch.transpose(logits, 1, 2)
+            losses = loss(logits, labels).cpu()
+            option_loss = torch.sum(losses, dim=-1).item()
+            option_losses[mc_i] = option_loss
+        ic(option_losses)
+        pred_i = int(torch.argmin(option_losses).item())
+        corr.append(pred_i == corr_i)
+    return float(np.mean(np.array(corr)))
