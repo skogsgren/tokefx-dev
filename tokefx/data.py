5207b39df792a97cedf5a1b776d59c9e6ffab537
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2026-02-23 16:50:05 +0100

    simplify to ease analyses

--- tokefx/data.py
+++ tokefx/data.py
@@ -1,51 +1,90 @@
-from abc import abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
+from typing import Iterator
 
-from conllu import parse_incr
-from tokenizers import Tokenizer
+import conllu
 from transformers import AutoTokenizer
 
 from icecream import ic
 
 
-class TokenList:
+@dataclass
+class PUDToken:
+    idx: int
+    form: str
+    upos: str
+    input_ids: list[int]
+    space_after: bool
+
+
+@dataclass
+class PUDSequence:
+    sentence_id: int
+    pud_tokens: list[PUDToken]
+
+    def get_text_til_i(self, idx: int):
+        text = ""
+        for i in range(idx):
+            text += self.pud_tokens[i].form
+            # since spaces are added after and not before tokens we have to
+            # make sure we don't add a space after the index we're after
+            if self.pud_tokens[i].space_after and i != idx - 1:
+                text += " "
+        return text
+
+
+class PUD_Data:
     def __init__(
         self,
-        data_file: Path,
-        tokenizer: Tokenizer,
-        min_tokens: int = 1,
-        min_bytes: int = 1,
-        max_tokens: int = 0,
+        datafp: Path,
+        tokenizer_spec: str,
+        context_window: int = 4096,
+        add_special_tokens: bool = False,
     ):
-        self.tokenizer = tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_spec)
+        self.context_window = context_window
+        self.datafp = datafp
+        self.add_special_tokens = add_special_tokens
 
-        # controls at least how many tokens/bytes each "Token" yielded should be
-        self.min_tokens = min_tokens
-        self.max_tokens = max_tokens
-        self.min_bytes = min_bytes
+    def _parse_seq(self, seq: conllu.models.TokenList) -> PUDSequence:
+        sequence_id = seq.metadata["parallel_id"]
+        pud_tokens = []
+        for i, tok in enumerate(seq):
+            space_after = (tok.get("misc") or {}).get("SpaceAfter") != "No"
+            input_ids = self.tokenizer(
+                tok["form"],
+                add_special_tokens=self.add_special_tokens,
+                truncation=True,
+                max_length=self.context_window,
+            )["input_ids"]
+            pud_tokens.append(
+                PUDToken(i, tok["form"], tok["upos"], input_ids, space_after)
+            )
+        return PUDSequence(sequence_id, pud_tokens)
 
-        self.token_list = self._parse_tokens(data_file)
+    def __iter__(self) -> Iterator[PUDSequence]:
+        text = self.datafp.read_text()
+        for sent_idx, seq in enumerate(conllu.parse(text)):
+            yield self._parse_seq(seq)
 
-    def __iter__(self):
-        for x in self.token_list:
-            yield x
 
-    def _parse_tokens(self, data_file: Path) -> Iterable:
-        with open(data_file) as f:
-            for seq in parse_incr(f):
-                for token in seq:
-                    if not (tokens := self._parse_token(str(token))):
+if __name__ == "__main__":
+    tokenizer_spec = "google/byt5-small"
+    tokenizer_spec = "gpt2"
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_spec)
+
+    for n in range(2, 5):
+        for lang in ["en", "zh", "tr"]:
+            length = 0
+            data = PUD_Data(
+                datafp=Path(f"./{lang}_pud-ud-test.conllu"),
+                tokenizer_spec=tokenizer_spec,
+            )
+            for i, x in enumerate(data):
+                for tok in x.pud_tokens:
+                    if len(tok.input_ids) != n:
                         continue
-                    yield (token["form"], tokens)
-
-    def _parse_token(self, token: str):
-        if len(token.encode("utf-8")) <= self.min_bytes:
-            return
-        sub_tokens = self.tokenizer.encode(token)
-        if len(sub_tokens) <= self.min_tokens:
-            return
-        if self.max_tokens != 0 and self.max_tokens <= len(sub_tokens):
-            return
-        return sub_tokens
+                    length += 1
+                    if length <= 2:
+                        ic(n, lang, tok.form, tok.input_ids)
+            print(f"({lang.upper()}, {n})\t{length}")
