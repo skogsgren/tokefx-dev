d29ab24cf8b7b1b96acc7b06029f3c1d64f1a431
Gustaf Gren <6107181+skogsgren@users.noreply.github.com>
2026-03-03 20:29:11 +0100

    add llama config

---
+++ slurm/attention_llama.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH --gpus-per-node=A40:1
+#SBATCH --nodes=1
+#SBATCH -t 0-01:00:0
+#SBATCH --output=logs/attention_llama/log-%j.out
+#SBATCH -A NAISS2025-22-601
+set -euo pipefail
+
+module purge
+if [ -z "${MIMER_DIR}" ]; then
+  echo "$(date) Error: env MIMER_DIR is not defined" >&2
+  exit 1
+fi
+
+echo "$(date) loading modules"
+module load "CUDA/12.6.0"
+module load "PyTorch/2.7.1-foss-2024a-CUDA-12.6.0"
+
+echo "$(date) activating venv"
+source "$MIMER_DIR/.venv/tokefx/bin/activate"
+
+echo "$(date) starting attention evaluation for Llama 3.2 models"
+cd "$MIMER_DIR/tokefx-dev/" || exit
+python3 scripts/attention.py configs/llama_config.toml
+echo "$(date) finished attention evaluation for Llama 3.2 models"
